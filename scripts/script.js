a9ef924f2f1f2619dd7a218f7310480c8a8d51fe
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-03-18 23:46:50 +0600

    delete button is added

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-const totalProducts = []
+let totalProducts = []
 const loadProduct = ()=>{
        const url = './scripts/product.json'
        fetch(url)
@@ -9,7 +9,7 @@ const loadProduct = ()=>{
          const div = document.createElement('div')
          const displayProducts = document.getElementById('displayproducts')
          div.innerHTML = `
-         <div onclick="products(event)" class="products rounded-2xl shadow-sm h-full">
+         <div id=${data.id} onclick="products(event)" class="products rounded-2xl shadow-sm h-full">
             <div class="product-image product-image inline-block"><img class="w-60 h-60" src="${data.image}" alt=""></div>
             <h2 class="product-name Bangla-font text-2xl font-semibold mt-3">${data.name}</h2>
             <p class="Bangla-font text-xl font-semibold mt-2"><span class="product-price">৳ ${data.price}</span>  <span class="unit">${data.unit}</span></p>
@@ -62,7 +62,7 @@ const loadProduct = ()=>{
          
           
          div.innerHTML = `
-         <div class="products rounded-2xl shadow-sm h-full">
+         <div id=${data.id} onclick="products(event)" class="products rounded-2xl shadow-sm h-full">
             <div class="product-image inline-block"><img class="w-60 h-60" src="${data.image}" alt=""></div>
             <h2 class="Bangla-font text-2xl font-semibold mt-3">${data.name}</h2>
             <p class="Bangla-font text-xl font-semibold mt-2"><span class="product-price">৳ ${data.price}</span> <span>${data.unit}</span></p>
@@ -96,14 +96,15 @@ const loadProduct = ()=>{
   const products = (event)=>{
     if(event.target.classList.contains('cartbtn')){
     const parentNode = event.currentTarget
+    const productID = parentNode.id
     const productImage = parentNode.querySelector('.product-image')
     const productName = parentNode.querySelector('.product-name').innerText
     const productPrice = parentNode.querySelector('.product-price').innerText
     const cartBtn = parentNode.querySelector('.cartbtn')
-    console.log(productPrice);
-    
+
     
     const productInfo = {
+      productID,
       productImage,
       productName,
       productPrice
@@ -121,8 +122,10 @@ const loadProduct = ()=>{
    document.getElementById('notProductadd').classList.add('hidden')
    document.getElementById('allcartproduct').classList.remove('hidden')
    document.getElementById('cartproductlist').classList.add('add-mt')
+   
     renderProduct()
     calculateTotalPrice()
+    
   }
 }
 
@@ -150,7 +153,6 @@ const loadProduct = ()=>{
       hero.classList.remove('hidden')
       allProduct.classList.remove('hidden')
       cart.classList.add('hidden')
-      
     })
 
    
@@ -159,32 +161,28 @@ const loadProduct = ()=>{
     const delivaryPrice = document.getElementById('delivarycharge').innerText
     const totalAdd = document.getElementById('totaladd').innerText
     const totalPrice = document.getElementById('totalprice')
-     const currentPrice = parseInt(productPrice)*parseInt(totalAdd) + parseInt(delivaryPrice) 
-        totalPrice.innerText = currentPrice
+    const currentPrice = parseInt(productPrice)*parseInt(totalAdd) + parseInt(delivaryPrice) 
+    totalPrice.innerText = currentPrice
    
     
 
-    const renderProduct = ()=>{
+  const renderProduct = ()=>{
   const cartContainer = document.getElementById('leftsidecart')
   cartContainer.innerHTML = ''
 
   for(let product of totalProducts){
+
     
     const div = document.createElement('div')
 
     div.innerHTML = `
-    <div class="rounded-2xl shadow p-6">
+    <div id=${product.productID} class="rounded-2xl shadow p-6 relative">
           ${product.productImage.outerHTML}
               <h1 class="font-semibold Bangla-font text-2xl">${product.productName}</h1>
               <h1 id="allproductprice" class="font-semibold Bangla-font text-lg">${product.productPrice}</h1>
-              <div class="join join-horizontal flex items-center border border-gray-100 w-fit rounded-2xl">
-                <button onclick="remove()" class="btn  join-item rounded-l-2xl bg-[#d7fcd5]">-</button>
-                    <h1 id="totaladd" class="mx-4">1</h1>
-                     <button onclick="add()" class="btn join-item rounded-r-2xl bg-[#d7fcd5]">+</button>
-                </div>
-                </div>
+              <button onclick="deleteProduct(${product.productID})" class="dltbtn absolute top-4 right-6 cursor-pointer"><i class="fa-solid fa-trash"></i></button>
+              </div>
     `
-
     cartContainer.appendChild(div)
   }
 }
@@ -192,32 +190,32 @@ const loadProduct = ()=>{
   let allProductPrice = document.getElementById('allproductprice')
  
     
-let totalCartProduct = document.getElementById('totaladd')
-    let currentCount = parseInt(document.getElementById('totaladd').innerText)
-    const remove = ()=>{
-      if(currentCount>1){
-        let totalAddProduct = 1
-        currentCount = currentCount -1
-        totalCartProduct.innerText = currentCount  
-        totalAddProduct = currentCount
-         const currentPrice = parseInt(productPrice)*totalAddProduct + parseInt(delivaryPrice) 
-        totalPrice.innerText = currentPrice
+// let totalCartProduct = document.getElementById('totaladd')
+//     let currentCount = parseInt(document.getElementById('totaladd').innerText)
+//     const remove = ()=>{
+//       if(currentCount>1){
+//         let totalAddProduct = 1
+//         currentCount = currentCount -1
+//         totalCartProduct.innerText = currentCount  
+//         totalAddProduct = currentCount
+//          const currentPrice = parseInt(productPrice)*totalAddProduct + parseInt(delivaryPrice) 
+//         totalPrice.innerText = currentPrice
     
-      }
-      else{
-        return
-      }
-    }
-    const add = ()=>{
-      let totalAddProduct = 1
+//       }
+//       else{
+//         return
+//       }
+//     }
+//     const add = ()=>{
+//       let totalAddProduct = 1
       
-      currentCount = currentCount +1
-      totalAddProduct = currentCount
-      const currentPrice = parseInt(productPrice)*totalAddProduct + parseInt(delivaryPrice) 
-      totalPrice.innerText = currentPrice
-      totalCartProduct.innerText = currentCount  
+//       currentCount = currentCount +1
+//       totalAddProduct = currentCount
+//       const currentPrice = parseInt(productPrice)*totalAddProduct + parseInt(delivaryPrice) 
+//       totalPrice.innerText = currentPrice
+//       totalCartProduct.innerText = currentCount  
       
-    }
+//     }
 
 const calculateTotalPrice = () => {
 
@@ -234,3 +232,12 @@ const calculateTotalPrice = () => {
   document.getElementById('totalprice').innerText = total + deliveryCharge
 }
 
+const deleteProduct = (id) => {
+  totalProducts = totalProducts.filter(product => product.productID != id)
+
+  renderProduct()
+  calculateTotalPrice()
+
+  document.getElementById('cartcount').innerText = totalProducts.length
+  document.getElementById('cartTextDropCount').innerText = totalProducts.length
+}
